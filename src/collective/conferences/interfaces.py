3234aea92893c595850e358a19d95a5be8932681
andreasma <maand@gmx.de>
2021-01-04 20:45:00 +0100

    Move all interfaces to a central interfaces file.

--- src/collective/conferences/interfaces.py
+++ src/collective/conferences/interfaces.py
@@ -1,8 +1,26 @@
 # -*- coding: utf-8 -*-
 """Module where all interfaces, events and exceptions live."""
 
+from plone.autoform.interfaces import IFormFieldProvider
+from plone.supermodel import model
+from zope.interface import Interface
+from zope.interface import provider
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 
 
 class ICollectiveConferencesLayer(IDefaultBrowserLayer):
     """Marker interface that defines a browser layer."""
+
+
+class IVotableLayer(Interface):
+    """Marker interface for the Browserlayer
+    """
+
+
+class IVotable(Interface):
+    pass
+
+
+@provider(IFormFieldProvider)
+class IVoting(model.Schema):
+    pass
