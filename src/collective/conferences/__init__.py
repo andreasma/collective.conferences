85020fd5375efd2db3ec1646bfb8cc7cc42c927a
andreasma <maand@gmx.de>
2021-01-30 22:42:49 +0100

    Add definitions for the custom permissions.

--- src/collective/conferences/__init__.py
+++ src/collective/conferences/__init__.py
@@ -3,3 +3,6 @@ from zope.i18nmessageid import MessageFactory
 
 
 _ = MessageFactory('collective.conferences')
+
+DoVote = 'collective.conferences: Do Vote'
+ViewVote = 'collective.conferences: View Vote'
